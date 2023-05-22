3e4b6ce48e04da275e9babaf340e35745084ae57
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-22 16:49:44 +0700

    menambahkan class ibu dan menambahkan void tambah anak dan void cetak anak

--- mainIA/Ibu.h
+++ mainIA/Ibu.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef Ibu_h
+#define Ibu_h
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n;
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
