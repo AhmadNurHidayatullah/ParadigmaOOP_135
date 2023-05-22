114514f1c6476201eb69417fbcfa337f54160ddd
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-22 17:14:18 +0700

    menambahkan class manusia

--- mainJM/manusia.h
+++ mainJM/manusia.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef manusia_h
+#define manusia_h
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varjantung;
+
+	manusia(string pNama)
+		: name(pNama) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << " mati\n";
+	}
+};
+#endif
