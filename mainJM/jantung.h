a5e57ae4a3abb86447bd348356abe303154dc701
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-22 17:09:36 +0700

    menambahkan class jantung

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef jantung_h
+#define jantung_h
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
